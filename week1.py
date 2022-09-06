d2ab3cd6fa5486f9aa27d6de00df4eabd37ff44c
YANG Qi <106226153+yqml@users.noreply.github.com>
2022-09-06 13:55:42 +0800

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,29 @@
+# import numpy as np
+# import collections
+
+# print('hello world')
+#
+# a = 1
+# b = 2.5
+# print('a+b = %d' % (a+b))
+# print('a+b = %f' % (a+b))
+# print('my name is %s' % ('yang杨'))
+
+# breakpoint()
+
+# for i in range(10):
+#     print(i)
+
+# import sys
+# print(dir(sys))
+
+# The definition of a function
+# def cmp(x,y):
+#     if x < y:
+#         return(-1)
+#     elif x == y:
+#         return(0)
+#     elif x > y:
+#         return(1)
+#
+# print(cmp(2,4))
